d35450da0c0e7d0aca5bf3ab0be605d4207fe304
BunniBooXx <140341233+BunniBooXx@users.noreply.github.com>
2023-11-08 19:38:40 -0500

    first commit

---
+++ src/Form.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState } from "react";
+
+
+export default function Form({onAdd}) {
+    const [taskName, setTaskName] = useState('');
+
+    function handleSubmit(ev) {
+        ev.preventDefault();
+        onAdd(taskName);
+        setTaskName('');
+    }
+
+  return (
+    <form onClick={handleSubmit}>
+        <button>+</button>
+        <input type="text" value={taskName} onChange={ev=> setTaskName(ev.target.value)} placeholder= "Enter Task"/>
+    </form>
+  );
+}
+
